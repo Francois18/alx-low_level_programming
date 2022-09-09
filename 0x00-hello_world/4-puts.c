cd81b5001d7060a1bda55c6ea3d39478d2b4082b
francois18 <francoiskalumuna18@gmail.com>
2022-09-09 14:43:01 +0000

    a C program that prints exactly "Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash 
-pust("\"Programming is like building a multilingual puzzle"); return (0)
+#include <stdio.h>
+
+/**
+ * main Entry point
+ * 
+ * Description: prints "Programming is like building a multilingula puzzle
+ *
+ * Return: Always 0 (success)
+ * /
+ 
+ int main(void)
+ {
+    pust("\"Programming is like building a multilingual puzzle");
+    return (0);
+ }
