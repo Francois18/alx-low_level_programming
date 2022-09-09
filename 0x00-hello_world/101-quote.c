9cfb9ea853d33a3089d35481d850c453832c838a
francois18 <francoiskalumuna18@gmail.com>
2022-09-09 21:23:08 +0000

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,14 @@
 /**
 * main - entry point
 *
-* Description: prints and that piece of art is useful\" - Dora Korpar, 2015-10-19
+* Description: prints and that piece of art is useful\" - Dora Korpar, 
+* 2015-10-19
 *
 * Return: 1
 */
 int main(void)
 {
-   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+		   59);
    return (1);
 }
