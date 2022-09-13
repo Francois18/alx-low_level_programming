758dc2d480937ab852c62a127020af526928a286
francois18 <francoiskalumuna18@gmail.com>
2022-09-13 19:46:49 +0000

    _putchar printed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(word[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
