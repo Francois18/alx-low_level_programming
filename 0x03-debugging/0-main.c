8705c20102bdcc696afbfaee780cedf5981344ac
francois18 <francoiskalumuna18@gmail.com>
2022-09-15 22:29:21 +0000

    stop the infinite loop

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,16 @@
 #include "main.h"
-/*
- * main - tests function that prints if integer is positive or negative
- *
- * Description:
- *
- * Return : 0 (Success)
- */
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_nagative(i);
+	positive_or_negative(i);
 
 	return (0);
 }
-
