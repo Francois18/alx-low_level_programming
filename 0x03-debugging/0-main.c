e1b44c297e8c5b81f501b18a1b630c4ca3abaaf1
francois18 <francoiskalumuna18@gmail.com>
2022-09-15 14:30:31 +0000

    if number is negaitve or positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * main - tests function that prints if integer is positive or negative
+ *
+ * Description:
+ *
+ * Return : 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_nagative(i);
+
+	return (0);
+}
+
