b2ee7a78a2c70af63cba8e695b5d42f71fdf6092
francois18 <francoiskalumuna18@gmail.com>
2022-09-20 23:33:36 +0000

    updates

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,8 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+int _putchar(char c);
+void puts_half(char *str);
 void puts2(char *str);
-
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
