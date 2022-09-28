6ecb7c33e038f45cbcd47cefe2dcca26c8b28ce1
francois18 <francoiskalumuna18@gmail.com>
2022-09-28 22:15:56 +0000

    function that return the length of a variable

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - returns the length of s
+ *@s: parameter
+ *
+ *Descritption  - return the length
+ *
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length;
+
+	length = strnlen(s);
+	retrn(1);
+}
