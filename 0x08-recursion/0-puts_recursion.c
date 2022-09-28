457640335bc4501f9a06c05a2062ae0842235160
francois18 <francoiskalumuna18@gmail.com>
2022-09-28 13:49:44 +0000

    print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - rpint a string followed by newline
+ *
+ *@s: parameteri
+ *
+ * Return void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
