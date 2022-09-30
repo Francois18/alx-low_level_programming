9fe6ff9e3572a90e6cc2e0341cf4852cb3a1eec4
francois18 <francoiskalumuna18@gmail.com>
2022-09-30 23:19:25 +0000

    adds positive number

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - entry point
+* @argc: parameter
+* @argv: parameter
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+	int result, i;
+
+	result = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= 0 && *argv[i] <= 9)
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d", result);
+	return (0);
+}
