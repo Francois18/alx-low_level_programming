955183a2a74c29eaa58ede88460bc9b2d68f779c
francois18 <francoiskalumuna18@gmail.com>
2022-09-30 13:45:27 +0000

    print the name of the file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * @argv: parameter
  * return int
 */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
