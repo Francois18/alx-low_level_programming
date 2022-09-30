0ba43937790936b1df346d659f7f48260a8e792a
francois18 <francoiskalumuna18@gmail.com>
2022-09-30 14:15:34 +0000

    argument passed on the CL counted

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: parameter
+ * @argv: parameter
+ * return int
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int i = argc - 1;
+
+	printf("%d\n", i);
+
+	return (0);
+}
