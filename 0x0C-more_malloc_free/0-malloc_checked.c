645236a21605ce502d2deaa9d3501fb66629bf25
francois18 <francoiskalumuna18@gmail.com>
2022-10-06 23:47:55 +0000

    function that allocate memory using mallc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* malloc_checked - allocates memory using malloc
+* @b: parameter
+* Return: void
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *varibale;
+
+	variable = malloc(b);
+	if (variableSpace == NULL)
+		return (98);
+	return (variable);
+}
