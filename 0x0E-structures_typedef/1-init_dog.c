06b09cf98509477ae46b1c5b0940c57c73a090b5
francois18 <francoiskalumuna18@gmail.com>
2022-10-10 16:04:05 +0000

    function prototypes added in the dog header file and initializing the variable of the type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - function declaration
+ * @name: paramter
+ * @age:parameter
+ * @owner: parameter
+ * @d: fourth parameter
+ *
+ * Description: initializing variable of type dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(d));
+	if ( d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+
+}
