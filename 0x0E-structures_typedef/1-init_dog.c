9388ac306c86b8e21d1d1c118e466e3aaea14b6b
francois18 <francoiskalumuna18@gmail.com>
2022-10-10 22:20:15 +0000

    struction definition

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,12 +14,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(d));
-	if ( d == NULL)
-		return;
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 
 }
