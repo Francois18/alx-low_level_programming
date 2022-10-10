c4815846c01eddb294bc009dd7e4123d2e4da762
francois18 <francoiskalumuna18@gmail.com>
2022-10-10 22:28:11 +0000

    function printinf the structure

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - function declaration
+ * @d: parameter one
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			prinf("Owner: %s\n", d->owner);
+	}
+}
