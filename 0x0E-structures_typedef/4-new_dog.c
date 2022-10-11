8063aa5a40c63df865609364f00a199ca007e61b
Francois18 <79960710+Francois18@users.noreply.github.com>
2022-10-11 23:06:56 +0300

    Function that create a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,75 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string parameter
+ * Return: pointer 
+ */
+
+char *_strdup(char *str)
+{
+	int length, i;
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	length = 0;
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+
+	arr = (char*) malloc(length * sizeof(char) + 1);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = str[i];
+	}
+	arr[i] = '\0';
+
+	return (arr);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: paramter
+ * @age: parameter
+ * @owner: parameter
+ * Return: new_dog (dog_t)
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+	char *doggo_name, *doggo_owner;
+
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
+
+	doggo_name = _strdup(name);
+	if (doggo_name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo_owner = _strdup(owner);
+	if (doggo_owner == NULL)
+	{
+		free(doggo_name);
+		free(doggo);
+		return (NULL);
+	}
+	doggo->name = doggo_name;
+	doggo->age = age;
+	doggo->owner = doggo_owner;
+	return (doggo);
+}
