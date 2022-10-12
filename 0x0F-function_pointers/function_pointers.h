32a693341ca679128d80e2070ee726ee613351dc
Francois18 <79960710+Francois18@users.noreply.github.com>
2022-10-12 21:58:23 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS.H
+#define FUNCTION_POINTERS.H
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
