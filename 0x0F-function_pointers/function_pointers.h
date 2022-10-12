712f993751cec56f503fb192db73d3fa88d36d06
francois18 <francoiskalumuna18@gmail.com>
2022-10-12 18:38:33 +0000

    protecting protoytypes from from double inclusion

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
+#ifndef FUNCTION_POINTERS.H
+#define FUNCTION_POINTERS.H
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
- int (*get_op_func(char *s))(int, int);
+int (*get_op_func(char *s))(int, int);
+#endif
