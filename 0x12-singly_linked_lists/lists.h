df84d8881dca1738ec2501c8b4bb4fb80a6db02a
francois18 <francoiskalumuna18@gmail.com>
2022-10-21 16:11:21 +0000

    header file updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,28 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int _putchar(char c);
-size_t print_list(const list_t *h);
+/**
+ * struct list_s - singly linkedlist
+ * @str: string parameter
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linkedlist node 
+ *  
+ */
 
-typedef struct list_t
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
+}list_t;
 
+int _putchar(char c);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
