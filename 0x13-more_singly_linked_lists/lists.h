fd40ad49ed5433f58408fc015d5ab54fecbccc77
francois18 <francoiskalumuna18@gmail.com>
2022-10-25 15:49:16 +0000

    header file updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,12 +12,12 @@
  * Description: singly linked list node structure
  *
  */
+
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 
 int _putchar (char c);
 size_t print_listint(const listint_t *h);
