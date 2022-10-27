887777862016ee6c2feb862058d2dddb1f3b035a
Francois18 <79960710+Francois18@users.noreply.github.com>
2022-10-28 00:37:19 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,104 +1,27 @@
-// #include "main.h"
-
-// /**
-//  * binary_to_uint - Converts a binary number to an unsigned int.
-//  * @b: A parameter pointer to a string of 0 and 1 chars.
-//  *
-//  * Return: If b is NULL or contains chars not 0 or 1 - 0.
-//  */
-// unsigned int binary_to_uint(const char *b)
-// {
-// 	unsigned int num = 0;
-// 	int len = 0;
-
-// 	if (b[len] == '\0')
-// 		return (0);
-
-// 	while ((b[len] == '0') || (b[len] == '1'))
-// 	{
-// 		num <<= 1;
-// 		num += b[len] - '0';
-// 		len++;
-// 	}
-
-// 	return (num);
-// }
-
 #include "main.h"
 
 /**
- * _atoi - converts chars to integer
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A parameter pointer to a string of 0 and 1 chars.
  *
- * @c: char to convert
- *
- * Return: converted integer
-*/
-unsigned int _atoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-
-/**
- * _strlen - gets the length of a string
- *
- * @str: string input
- *
- * Return: string length
-*/
-unsigned int _strlen(const char *str)
-{
-	unsigned int index = 0;
-
-	while (str[index] != '\0')
-		index++;
-	return (index);
-}
-
-/**
- * binary_to_uint - a function that converts a binary number
- *                  to an unsigned int
- *
- * @b: string that contains 0 and 1 characters
- *
- * Return: the number converted or 0
- *         if @b contains a character
- *         that is not 0 or 1 or when
- *         @b is null
-*/
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	int index;
-	unsigned int result = 0, base2 = 1,  num = 0;
+	unsigned int num = 0;
+	int len = 0;
 
-	/*if b is NULL return 0*/
-	if (b == NULL)
+	if (b[len] == '\0')
 		return (0);
 
-	#ifdef DEBUG /*print debug statement*/
-	printf("String is %s, and length is %u.\n", b, _strlen(b));
-	#endif
-
-	/*iterate through string*/
-	for (index = _strlen(b) - 1; index >= 0; index--)
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-		num = _atoi(b[index]); /*convert char to number*/
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
-		#endif
-
-		/*if number is not 0 or 1 return 0*/
-		if (num != 0 && num != 1)
-			return (0);
-
-		result += num * base2; /*enable debug to see it in action*/
-		base2 *= 2;
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Result is %u.\n", result);
-		#endif
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
 	}
 
-	return (result);
-
+	return (num);
 }
+
+
